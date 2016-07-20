b59d0b9bf23c6e1502055a158799c17a70963ffc
Ryan Lau <rylau@thoughtworks.com>
2016-07-20 11:03:15 -0500

    Create multiple heroes in the app

--- public/app/app.component.js
+++ public/app/app.component.js
@@ -11,7 +11,7 @@ System.register(['angular2/core'], function(exports_1, context_1) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
     var core_1;
-    var Hero, AppComponent;
+    var Hero, HEROES, AppComponent;
     return {
         setters:[
             function (core_1_1) {
@@ -24,18 +24,31 @@ System.register(['angular2/core'], function(exports_1, context_1) {
                 return Hero;
             }());
             exports_1("Hero", Hero);
+            HEROES = [
+                { id: 11, name: 'Mr. Nice' },
+                { id: 12, name: 'Narco' },
+                { id: 13, name: 'Bombasto' },
+                { id: 14, name: 'Celeritas' },
+                { id: 15, name: 'Magneta' },
+                { id: 16, name: 'RubberMan' },
+                { id: 17, name: 'Dynama' },
+                { id: 18, name: 'Dr IQ' },
+                { id: 19, name: 'Magma' },
+                { id: 20, name: 'Tornado' }
+            ];
             AppComponent = (function () {
                 function AppComponent() {
                     this.title = 'Tour of Heroes';
-                    this.hero = {
-                        id: 1,
-                        name: 'Windstorm'
-                    };
+                    this.heroes = HEROES;
                 }
+                AppComponent.prototype.onSelect = function (hero) {
+                    this.selectedHero = hero;
+                };
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "\n  \t\t\t<h1>{{title}}</h1>\n  \t\t\t<h2>{{hero.name}} Details!</h2>\n  \t\t\t<div>\t\n  \t\t\t\t<label>id: </label>{{hero.id}}\n\t\t\t</div>\n\t\t\t<div>\n\t\t\t\t<label>name: </label>{{hero.name}}\n\t\t\t\t<input [(ngModel)]=\"hero.name\" placeholder=\"name\">\n\t\t\t</div>"
+                        template: "\n\t\t\t<h1>{{title}}</h1>\n\t\t\t<h2>My Heroes</h2>\n\t\t\t<ul class=\"heroes\">\t\n\t\t\t  <li *ngFor=\"let hero of heroes\" [class.selected]=\"hero === selectedHero\" (click)=\"onSelect(hero)\">\n\t\t\t  \t<span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n\t\t\t  </li>\n\t\t\t</ul>\n\t\t\t<div *ngIf=\"selectedHero\">\n\t\t\t\t<h2>{{selectedHero.name}} Details!</h2>\n\t\t\t\t<div>\t\n\t\t\t\t\t<label>id: </label>{{selectedHero.id}}\n\t\t\t\t</div>\n\t\t\t\t<div>\n\t\t\t\t<label>name: </label>{{selectedHero.name}}\n\t\t\t\t<input [(ngModel)]=\"selectedHero.name\" placeholder=\"name\">\n\t\t\t\t</div>\n\t\t\t</div>",
+                        styles: ["\n\t\t\t  .selected {\n\t\t\t    background-color: #CFD8DC !important;\n\t\t\t    color: white;\n\t\t\t  }\n\t\t\t  .heroes {\n\t\t\t    margin: 0 0 2em 0;\n\t\t\t    list-style-type: none;\n\t\t\t    padding: 0;\n\t\t\t    width: 15em;\n\t\t\t  }\n\t\t\t  .heroes li {\n\t\t\t    cursor: pointer;\n\t\t\t    position: relative;\n\t\t\t    left: 0;\n\t\t\t    background-color: #EEE;\n\t\t\t    margin: .5em;\n\t\t\t    padding: .3em 0;\n\t\t\t    height: 1.6em;\n\t\t\t    border-radius: 4px;\n\t\t\t  }\n\t\t\t  .heroes li.selected:hover {\n\t\t\t    background-color: #BBD8DC !important;\n\t\t\t    color: white;\n\t\t\t  }\n\t\t\t  .heroes li:hover {\n\t\t\t    color: #607D8B;\n\t\t\t    background-color: #DDD;\n\t\t\t    left: .1em;\n\t\t\t  }\n\t\t\t  .heroes .text {\n\t\t\t    position: relative;\n\t\t\t    top: -3px;\n\t\t\t  }\n\t\t\t  .heroes .badge {\n\t\t\t    display: inline-block;\n\t\t\t    font-size: small;\n\t\t\t    color: white;\n\t\t\t    padding: 0.8em 0.7em 0 0.7em;\n\t\t\t    background-color: #607D8B;\n\t\t\t    line-height: 1em;\n\t\t\t    position: relative;\n\t\t\t    left: -1px;\n\t\t\t    top: -4px;\n\t\t\t    height: 1.8em;\n\t\t\t    margin-right: .8em;\n\t\t\t    border-radius: 4px 0 0 4px;\n\t\t\t  }\n\t\t\t"]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
