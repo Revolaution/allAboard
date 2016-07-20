6960b6b1e5c29388b75c82746522fee8c165f0b1
Ryan Lau <rylau@thoughtworks.com>
2016-07-20 10:04:06 -0500

    two way data binding of hero name

--- public/app/app.component.js
+++ public/app/app.component.js
@@ -11,22 +11,31 @@ System.register(['angular2/core'], function(exports_1, context_1) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
     var core_1;
-    var AppComponent;
+    var Hero, AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
             }],
         execute: function() {
+            Hero = (function () {
+                function Hero() {
+                }
+                return Hero;
+            }());
+            exports_1("Hero", Hero);
             AppComponent = (function () {
                 function AppComponent() {
                     this.title = 'Tour of Heroes';
-                    this.hero = 'Windstorm';
+                    this.hero = {
+                        id: 1,
+                        name: 'Windstorm'
+                    };
                 }
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: '<h1>{{title}}</h1><h2>{{hero}} Details!</h2>'
+                        template: "\n  \t\t\t<h1>{{title}}</h1>\n  \t\t\t<h2>{{hero.name}} Details!</h2>\n  \t\t\t<div>\t\n  \t\t\t\t<label>id: </label>{{hero.id}}\n\t\t\t</div>\n\t\t\t<div>\n\t\t\t\t<label>name: </label>{{hero.name}}\n\t\t\t\t<input [(ngModel)]=\"hero.name\" placeholder=\"name\">\n\t\t\t</div>"
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
--- public/app/app.component.ts
+++ public/app/app.component.ts
@@ -2,12 +2,32 @@ import {Component, OnInit} from 'angular2/core'
 import {RouteParams} from 'angular2/router' 
 import {Http, HTTP_PROVIDERS} from 'angular2/http'; 
 
+export class Hero {
+	id: number;
+	name: string;
+}
+
 @Component({
   selector: 'my-app',
-  template: '<h1>{{title}}</h1><h2>{{hero}} Details!</h2>'
+  template: `
+  			<h1>{{title}}</h1>
+  			<h2>{{hero.name}} Details!</h2>
+  			<div>	
+  				<label>id: </label>{{hero.id}}
+			</div>
+			<div>
+				<label>name: </label>{{hero.name}}
+				<input [(ngModel)]="hero.name" placeholder="name">
+			</div>`
 })
 
 export class AppComponent { 
 	title = 'Tour of Heroes';
-	hero = 'Windstorm'
+	hero: Hero = {
+		id: 1,
+		name: 'Windstorm'
+	}
 }
+
+
+
