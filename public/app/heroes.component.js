403b042d70e1e309d446664d01f0776707301646
Ryan Lau <rylau@thoughtworks.com>
2016-07-21 14:34:09 -0500

    heroes can be added and deleted in heroes component

--- public/app/heroes.component.js
+++ public/app/heroes.component.js
@@ -11,11 +11,13 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var router_1 = require('@angular/router');
 var hero_service_1 = require('./hero.service');
+var hero_detail_component_1 = require('./hero-detail.component');
 var HeroesComponent = (function () {
     function HeroesComponent(router, heroService) {
         this.router = router;
         this.heroService = heroService;
         this.title = 'Tour of Heroes';
+        this.addingHero = false;
     }
     HeroesComponent.prototype.ngOnInit = function () {
         this.getHeroes();
@@ -23,6 +25,29 @@ var HeroesComponent = (function () {
     HeroesComponent.prototype.getHeroes = function () {
         var _this = this;
         this.heroService.getHeroes().then(function (heroes) { return _this.heroes = heroes; });
+    };
+    HeroesComponent.prototype.addHero = function () {
+        this.addingHero = true;
+        this.selectedHero = null;
+    };
+    HeroesComponent.prototype.deleteHero = function (hero, event) {
+        var _this = this;
+        event.stopPropagation();
+        this.heroService
+            .delete(hero)
+            .then(function (res) {
+            _this.heroes = _this.heroes.filter(function (h) { return h !== hero; });
+            if (_this.selectedHero === hero) {
+                _this.selectedHero = null;
+            }
+        })
+            .catch(function (error) { return _this.error = error; });
+    };
+    HeroesComponent.prototype.close = function (savedHero) {
+        this.addingHero = false;
+        if (savedHero) {
+            this.getHeroes();
+        }
     };
     HeroesComponent.prototype.onSelect = function (hero) {
         this.selectedHero = hero;
@@ -34,7 +59,8 @@ var HeroesComponent = (function () {
         core_1.Component({
             selector: 'my-heroes',
             templateUrl: 'app/heroes.component.html',
-            styleUrls: ['app/heroes.component.css']
+            styleUrls: ['app/heroes.component.css'],
+            directives: [hero_detail_component_1.HeroDetailComponent]
         }), 
         __metadata('design:paramtypes', [router_1.Router, hero_service_1.HeroService])
     ], HeroesComponent);
--- public/app/heroes.component.ts
+++ public/app/heroes.component.ts
@@ -2,19 +2,22 @@ import {Component, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 
 import { Hero } from './hero';
-import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @Component({
 	selector: 'my-heroes',
 	templateUrl: 'app/heroes.component.html',
-	styleUrls:  ['app/heroes.component.css']
+	styleUrls:  ['app/heroes.component.css'],
+	directives: [HeroDetailComponent]
 })
 
 export class HeroesComponent implements OnInit {
 	title = 'Tour of Heroes';
 	heroes: Hero[];
 	selectedHero: Hero;
+	addingHero = false;
+	error: any;
 
 	constructor(private router: Router, private heroService: HeroService) {	}
 
@@ -24,7 +27,30 @@ export class HeroesComponent implements OnInit {
 
 	getHeroes() {
     	this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  	}
+  }
+
+	addHero() {
+	  this.addingHero = true;
+	  this.selectedHero = null;
+	}
+
+	deleteHero(hero: Hero, event: any) {
+	  event.stopPropagation();
+	  this.heroService
+	      .delete(hero)
+	      .then(res => {
+	        this.heroes = this.heroes.filter(h => h !== hero);
+	        if (this.selectedHero === hero) {
+						this.selectedHero = null;
+					}
+	      })
+	      .catch(error => this.error = error);
+	}
+
+	close(savedHero: Hero) {
+	  this.addingHero = false;
+	  if (savedHero) { this.getHeroes(); }
+	}
 
 	onSelect(hero: Hero) {
 		this.selectedHero = hero;
