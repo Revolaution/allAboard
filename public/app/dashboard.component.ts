261d71866b4cb0d594c28b7f0c2d56e83a276a64
Ryan Lau <rylau@thoughtworks.com>
2016-07-21 19:45:51 -0500

    Dashboard utilizing search

--- public/app/dashboard.component.ts
+++ public/app/dashboard.component.ts
@@ -3,22 +3,24 @@ import { Router } from '@angular/router';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
+import { HeroSearchComponent } from './hero-search.component'
 
 @Component({
   selector: 'my-dashboard',
   templateUrl: 'app/dashboard.component.html',
-  styleUrls: ['app/dashboard.component.css']
+  styleUrls: ['app/dashboard.component.css'],
+  directives: [HeroSearchComponent]
 })
 export class DashboardComponent {
 	heroes: Hero[] = [];
-	
+
 	constructor(private router: Router, private heroService: HeroService) { }
-	
+
 	ngOnInit() {
 		this.heroService.getHeroes()
 		.then(heroes => this.heroes = heroes.slice(1, 5));
 	}
-	
+
 	gotoDetail(hero: Hero) {
 		let link = ['detail', hero.id];
 		this.router.navigate(link);
